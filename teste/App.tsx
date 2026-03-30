abc3f9157fca8031a189d7b9d06503a7ddd700c5
Mateus Samuel de Oliveira Félix <20231148060009@ifrn.local>
2026-03-30 10:18:42 -0300

    Resposta do Exercício 1 - Criação do componente 'Carteira'

--- teste/App.tsx
+++ teste/App.tsx
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
+// 1. Importe o seu componente (ajuste o caminho se necessário)
+import Carteira from './components/carteira'; 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      {/* 2. Coloque o componente aqui no lugar do <Text> antigo */}
+      <Carteira />
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +18,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff', // Cor de fundo da tela inteira
+    alignItems: 'center',    // Centraliza o card horizontalmente
+    justifyContent: 'center', // Centraliza o card verticalmente
   },
 });
